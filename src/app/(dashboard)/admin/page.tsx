cefcc5a17fb0bc7b87eb2f635fba21d821931167
Noor <thenoorulhassan.dev@gmail.com>
2026-08-09 19:31:56 +0500

    Navbar | Charts | Menu

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,35 @@
+import CountChart from '@/components/CountChart'
+import UserCard from '@/components/UserCard'
 import React from 'react'
 
-const AdminPage=()=> {
+const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+
+      {/*Left*/}
+      <div className='w-full lg:w-2/3'>
+        {/*User Card*/}
+        <div className='flex justify-between gap-4'>
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/*MIDDLE Charts*/}
+        <div className='flex gap-4 flex-col lg:flex-row mt-4'>
+          {/*Count Chart*/}
+          <div className='w-full lg:w-1/3 h-[450px]'>
+            <CountChart />
+          </div>
+          {/*Bottom chart*/}
+          <div className='w-full lg:w-2/3 h-[450px]'>
+
+          </div>
+        </div>
+      </div>
+      {/*Right*/}
+      <div className='w-full lg:w-1/3'></div>
+    </div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,12 +17,12 @@ const Navbar = () => (
                 <Image src={"/announcement.png"} alt={"message"} height={20} width={20} />
                 <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-600 text-white rounded-full text-xs'>1</div>
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col hover:cursor-pointer'>
                 <span className='text-xs leading-3 font-medium'>John Doe</span>
                 <span className='text-[10px] text-gray-500 text-right'>Admin</span>
             </div>
             <div>
-                <Image src={"/avatar.png"} alt={""} height={36} width={36} className="rounded-full" />
+                <Image src={"/avatar.png"} alt={""} height={36} width={36} className="rounded-full hover:cursor-pointer" />
             </div>
         </div>
     </div>
