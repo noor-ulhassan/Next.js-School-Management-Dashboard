96b946250bead8998707b576fb30db187dcfa79b
Noor <thenoorulhassan.dev@gmail.com>
2026-08-11 22:30:15 +0500

    Built respective Components

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,8 @@
+import Announcements from '@/components/Announcements'
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import EventCalendar from '@/components/EventCalendar'
+import EventList from '@/components/EventList'
 import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
@@ -34,7 +37,11 @@ const AdminPage = () => {
         </div>
       </div>
       {/*Right*/}
-      <div className='w-full lg:w-1/3'></div>
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+        <EventCalendar />
+        <EventList />
+        <Announcements />
+      </div>
     </div>
   )
 }
---
+++ src/components/EventCalendar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const EventCalendar = () => {
+    const [value, onChange] = useState<Value>(new Date());
+
+    return <Calendar onChange={onChange} value={value} />;
+};
+
+export default EventCalendar;
